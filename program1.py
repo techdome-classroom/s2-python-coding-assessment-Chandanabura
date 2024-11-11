e6abf5cdf445cfc752ba4c51388dc16f0c3b2735
Chandan Bura <chandanabura123@gmail.com>
2024-11-11 15:34:19 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,24 +1,12 @@
-# class Solution(object):
-#     def isValid(self, s):
-#         """
-#         :type s: str
-#         :rtype: bool
-#         """
-#         pass
-
 class Solution(object):
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        # Dictionary to hold matching pairs
         bracket_map = {')': '(', '}': '{', ']': '['}
         stack = []
-        
-        # Iterate over each character in the string
         for char in s:
-            # If the character is a closing bracket
             if char in bracket_map:
                 # Pop from stack if stack is not empty, else assign a dummy value
                 top_element = stack.pop() if stack else '#'
