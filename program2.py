e5053ae982b165be81d1c35a3ba24109a9f6542a
Chandan Bura <chandanabura123@gmail.com>
2024-11-11 15:33:46 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -12,11 +12,8 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # Check for an empty string
         if not s:
             return 0
-        
-        # Mapping of Roman numerals to integer values
         roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         total = 0
         n = len(s)
